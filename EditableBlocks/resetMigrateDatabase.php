5de6ec303dcf79219a63af72decc50671cf1166b
Pekka <nospam@astudios.org>
2015-10-14 22:33:31 +0300

    Migration!

---
+++ EditableBlocks/resetMigrateDatabase.php
@@ -0,0 +1,25 @@
+<?php
+
+$databasePath = __DIR__ . '/app/editable_blocks.sqlite';
+
+if (file_exists($databasePath))
+{
+    echo 'Deleting current database file...' . PHP_EOL;
+    unlink($databasePath);
+    echo 'Done.' . PHP_EOL;
+}
+
+echo 'Creating new database file...' . PHP_EOL;
+touch($databasePath);
+echo 'Done.' . PHP_EOL;
+
+$sql = 'CREATE TABLE [blocks] (
+[id] CHAR(50) NOT NULL,
+[created_at] DATETIME,
+[updated_at] DATETIME,
+[content] TEXT);
+
+CREATE INDEX [pk] ON [blocks] ([id]);';
+
+$db = new PDO('sqlite:' . $databasePath);
+$db->exec($sql);
